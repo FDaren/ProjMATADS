af133ab7d25104a95e4e0491838ebe7e8c593901
FDaren <135279047+FDaren@users.noreply.github.com>
2023-07-16 20:45:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ import yfinance as yf
 import streamlit as st
 import plotly.graph_objects as go
 
-# Titulo do APP
-
+# Definindo as configurações iniciais do aplicativo Streamlit
 st.title('Stock History App')
-
-# Criando o SideBar
 st.sidebar.title('Selecione o stock')
 ticker_symbol = st.sidebar.text_input('stock', 'AAPL', max_chars=10)
 
-# Baixando os dados
+# Baixando os dados para o primeiro gráfico
+data = yf.download(ticker_symbol, start='2020-01-01', end='2023-06-26')
 
-data = yf.download(ticker_symbol, start= '2020-01-01', end = '2023-06-26')
+# Exibindo os dados em forma de tabela
+st.subheader('Histórico')
+st.dataframe(data)
 
-# Exibir os dados
-
-st.subheader ('Histórico')
-st.dataframe (data)
-
-# Exibir o gráfico
+# Criando o primeiro gráfico
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data.index, y= data['Close'], name = 'Fechamento'))
-fig.update_layout(title = f'{ticker_symbol}', xaxis_title = 'Data', yaxis_title = 'Preço')
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Fechamento'))
+fig.update_layout(title=f'{ticker_symbol}', xaxis_title='Data', yaxis_title='Preço')
 st.plotly_chart(fig)
+
+# Baixando os dados para o segundo gráfico
+data2 = yf.download(ticker_symbol, start='2020-01-01', end='2023-06-26')
+
+# Criando o segundo gráfico
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x=data2.index, y=data2['Volume'], name='Volume'))
+fig2.update_layout(title=f'{ticker_symbol} - Volume', xaxis_title='Data', yaxis_title='Volume')
+st.plotly_chart(fig2)
